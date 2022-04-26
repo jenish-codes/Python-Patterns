d03463e55c50cab0f6b8eaae114be6910b20870f
jenish-codes <jenish.codes@gmail.com>
2022-04-26 20:18:36 +0530

    another commit on 26 april 2022

--- 01-thirty-patterns/30-patterns.py
+++ 01-thirty-patterns/30-patterns.py
@@ -1,5 +1,5 @@
 
-##square pattern
+#1 square pattern
 print("Square Pattern")
 for i in range(0,5):       #creating list of rows
     for j in range(0,5):   #create a list of colums
@@ -16,7 +16,7 @@ for i in range(0,5):
 
 
 
-##hollow square
+#2 hollow square
 print("Hollow Square")
 size=5
 for i in range(size):
@@ -40,7 +40,7 @@ for i in range(size):
 
 
 
-##Left triangle
+#3 Left triangle
 print("\nleft triangle")
 n=5
 for i in range(1,n+1):
@@ -57,7 +57,7 @@ for i in range(1,n+1):
 
 
 
-##right triangle
+#4 right triangle
 print("\n Right Triangle \n")
 size = 5
 for i in range(size):
@@ -76,9 +76,106 @@ for i in range(1,size+1):
 
 
 
-##Left downward triangle
+#5 Left downward triangle
 
 print("\n left downward triangle \n")
+n=5
+for i in range(n):
+    for j in range(n-i):
+        print("*",end='')
+    print()
+
+
+for i in range(n):
+    print('*'*(n-i))
+
+
+
+
+
+
+
+#6 right downward triangle
+
+print('\n RIght Downward triangle \n')
+
+n=5
+for i in range(n):
+    for j in range(i):
+        print(" ",end='')
+    for k in range(n-i):
+        print('*',end='')
+    print()
+
+
+
+
+#7 Hollow triangle
+print('\n Hollow Triangle \n')
+
+n=6
+for i in range(1,n+1):
+    for j in range(i):
+        #print star only at start and end of the row
+        if j ==0 or j==i-1:
+            print('*',end='')
+        #print only star if its last row
+        else:
+            if i != n:
+                print(' ', end='')
+            else:
+                print('*',end='')
+    print()
+
+
+
+#Pyramid Pattern
+print('\n Pyramid Pattern \n')
+
+n=5
+for i in range(n):
+    print(" "*(n-i-1)+'*'*(2*i+1))
+
+print('\n')
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(' ',end='')
+    for k in range(2*i+1):
+        print('*',end='')
+    print()
+
+
+
+
+
+#hollow pyramid
+print('\n Hollow Pyramid \n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
